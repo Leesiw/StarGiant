9ae9a3e5d4c777eff2beaa6bc7338c427d14a2d1
백아울 <luna8297@naver.com>
2022-08-01 00:21:54 +0900

    가속도 수정

--- star/Player.cpp
+++ star/Player.cpp
@@ -55,45 +55,45 @@ void CPlayer::ReleaseShaderVariables()
 
 void CPlayer::Move(DWORD dwDirection, float fDistance, bool bUpdateVelocity)
 {
-	//if (dwDirection)
-	//{
-	//	//XMFLOAT3 xmf3Shift = XMFLOAT3(0, 0, 0);
-	//	//가속도 최대크기를 정해놓고, 1) 항상 가속도는 줄어든다. 2) 최대크기 이상으로 커지지 않음 . 을 구현해야 
-	//	//will.y = (1 - 0.5) * will.y + 0.5 * (will.y+will.velocity_y*game_framework.frame_time)
-	//	//velocity_y -> distance를 뜻함... 
-
-	//	//m_xmf3Shift = Vector3::Add(XMFLOAT3(0, 0, 0), m_xmf3Shift, 0.95);
-	//	if (dwDirection & DIR_FORWARD) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Look, fDistance);
-	//	if (dwDirection & DIR_BACKWARD) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Look, -fDistance);
-	//	if (dwDirection & DIR_RIGHT) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Right, fDistance);
-	//	if (dwDirection & DIR_LEFT) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Right, -fDistance);
-	//	if (dwDirection & DIR_UP) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Up, fDistance);
-	//	if (dwDirection & DIR_DOWN) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Up, -fDistance);
-
-	//	Move(m_xmf3Shift, bUpdateVelocity);
-	//}
-
-
 	if (dwDirection)
 	{
-		XMFLOAT3 xmf3Shift = XMFLOAT3(0, 0, 0);
+		//XMFLOAT3 xmf3Shift = XMFLOAT3(0, 0, 0);
+		//가속도 최대크기를 정해놓고, 1) 항상 가속도는 줄어든다. 2) 최대크기 이상으로 커지지 않음 . 을 구현해야 
+		//will.y = (1 - 0.5) * will.y + 0.5 * (will.y+will.velocity_y*game_framework.frame_time)
+		//velocity_y -> distance를 뜻함... 
 
-
-		if (dwDirection & DIR_FORWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance);
-		if (dwDirection & DIR_BACKWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance);
+		//m_xmf3Shift = Vector3::Add(XMFLOAT3(0, 0, 0), m_xmf3Shift, 0.95);
+		if (dwDirection & DIR_FORWARD) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Look, fDistance);
+		if (dwDirection & DIR_BACKWARD) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Look, -fDistance);
 		if (dwDirection & DIR_RIGHT) Rotate(0.0f, 0.5f, 0.0f);
-	//	if (dwDirection & DIR_RIGHT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance);
-
 		if (dwDirection & DIR_LEFT) Rotate(0.0f, -0.5f, 0.0f);
-		//if (dwDirection & DIR_LEFT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance);
+		if (dwDirection & DIR_UP) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Up, fDistance);
+		if (dwDirection & DIR_DOWN) m_xmf3Shift = Vector3::Add(m_xmf3Shift, m_xmf3Up, -fDistance);
 
-		if (dwDirection & DIR_UP) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, fDistance);
-		if (dwDirection & DIR_DOWN) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, -fDistance);
-
-		Move(xmf3Shift, bUpdateVelocity);
-
-
+		Move(m_xmf3Shift, bUpdateVelocity);
 	}
+
+
+	//if (dwDirection)
+	//{
+	//	XMFLOAT3 xmf3Shift = XMFLOAT3(0, 0, 0);
+
+
+	//	if (dwDirection & DIR_FORWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance);
+	//	if (dwDirection & DIR_BACKWARD) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance);
+	//	if (dwDirection & DIR_RIGHT) Rotate(0.0f, 0.5f, 0.0f);
+	////	if (dwDirection & DIR_RIGHT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance);
+
+	//	if (dwDirection & DIR_LEFT) Rotate(0.0f, -0.5f, 0.0f);
+	//	//if (dwDirection & DIR_LEFT) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance);
+
+	//	if (dwDirection & DIR_UP) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, fDistance);
+	//	if (dwDirection & DIR_DOWN) xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Up, -fDistance);
+
+	//	Move(xmf3Shift, bUpdateVelocity);
+
+
+	//}
 }
 
 //void CPlayer::Move(const XMFLOAT3& xmf3Shift, bool bUpdateVelocity)
@@ -118,10 +118,11 @@ void CPlayer::Move(const XMFLOAT3& xmf3Shift, bool bUpdateVelocity)
 	}
 	else
 	{
+		m_xmf3Shift = Vector3::Add(XMFLOAT3(0, 0, 0), m_xmf3Shift, 0.95);
 		m_xmf3Position = Vector3::Add(m_xmf3Position, xmf3Shift);
 		m_pCamera->Move(xmf3Shift);
 	}
-
+	m_xmf3Shift = xmf3Shift;
 	//if (bUpdateVelocity) m_xmf3Velocity = Vector3::Add(m_xmf3Velocity, xmf3Shift);
 	//else m_xmf3Position = Vector3::Add(m_xmf3Position, xmf3Shift);
 }
