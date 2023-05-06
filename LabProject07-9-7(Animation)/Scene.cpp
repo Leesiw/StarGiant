713d61d70cff1f375da0026120a469178a6549f8
Lee Si Woo <asonaso@kpu.ac.kr>
2023-05-06 22:13:49 +0900

    클라 적 움직임 일단 제거

--- LabProject07-9-7(Animation)/Scene.cpp
+++ LabProject07-9-7(Animation)/Scene.cpp
@@ -1113,7 +1113,7 @@ void CScene::AnimateObjects(float fTimeElapsed)
 		}
 	}
 
-
+	/*
 	for (int i = 0; i < ENEMIES; ++i) {
 		if (m_ppEnemies[i] && m_ppEnemies[i]->isAlive){// && !m_ppEnemies[i]->isUpdate) {
 			m_ppEnemies[i]->AI(fTimeElapsed, m_pPlayer[0]->GetLook(), m_pPlayer[0]->GetPosition());
@@ -1124,7 +1124,7 @@ void CScene::AnimateObjects(float fTimeElapsed)
 				m_ppEnemies[i]->isUpdate = false;
 			}
 		}
-	}
+	}*/
 
 	for (int i = 0; i < UI_CNT; i++)
 	{
