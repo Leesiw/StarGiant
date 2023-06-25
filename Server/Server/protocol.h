3e4e5b59ba3fa0d607cde41cda2e89eae483944b
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2023-06-26 08:47:20 +0900

    방 크기 돌려놓음

--- Server/Server/protocol.h
+++ Server/Server/protocol.h
@@ -31,7 +31,7 @@ constexpr char SPRITE_CNT = 6;
 
 constexpr char MAX_ITEM = 15;	// 보석 최대 레벨?
 
-constexpr int MAX_ROOM = 1;
+constexpr int MAX_ROOM = 1000;
 constexpr int MAX_USER = MAX_ROOM * 3;
 
 constexpr int PORT_NUM = 4000;
